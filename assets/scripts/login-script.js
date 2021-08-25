6d1523a0fb7f7c3649cb0f6c1e32c6bb9e5230b7
Philip Imanuel Balangkaehe <87025337+pib0student@users.noreply.github.com>
2021-08-25 21:19:26 +0700

    Update login-script.js
    Kembali ke semula

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,55 +1,19 @@
-/** @module Login-Script */
-/** 
-* Membuat variabel loginFormElement untuk tampilan form.
-* @constant {HTMLElement}
-*/
+/* Comment : Membuat variabel untuk setiap elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
- 
- 
-/** 
-* Membuat variabel inputEmailElement untuk tampilan input email.
-* @constant {HTMLElement}
-*/
 const inputEmailElement = document.querySelector('#inputEmail');
- 
- 
-/** 
-* Membuat variabel inputPasswordElement untuk tampilan input password.
-* @constant {HTMLElement}
-*/
 const inputPasswordElement = document.querySelector('#inputPassword');
  
- 
-/** 
- * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
- * @constant {string}
- */
+/* Comment : Membuat variabel untuk menyimpan informasi email dan password. */
 const expectedEmail = 'admin@dicoding.com';
- 
- 
-/** 
- * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
- * @constant {string}
- */
-const expectedPassword = 'superpassword'
- 
+const expectedPassword = 'superpassword';
  
 /* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   
-  /** 
-   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
-   * @constant {string}
-   */
+  /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
   const email = inputEmailElement.value;
-  
-  /** 
-   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
-   * @constant {string}
-   */
   const password = inputPasswordElement.value;
- 
  
   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
